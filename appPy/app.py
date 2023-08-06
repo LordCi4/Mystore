e09c8e79815914856e2214bbf3482749feabf8fa
LordCi4 <ramosubaldino5@gmail.com>
2023-08-06 09:51:04 -0500

    app save prodct 2 dtdbs

--- appPy/app.py
+++ appPy/app.py
@@ -1,12 +1,49 @@
-from flask import Flask,render_template
+import mysql.connector
+from flask import Flask,request,render_template
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "paskall14",
+    database = "storedb"
+)
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM shop")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
+
+#------ADD VAlUES TO DATABASE
+
+@app.route("/add", methods = ['POST', 'GET'])
+def add():
+    adding()
+    return render_template("add.html")
+  
+
+def adding():
+    if request.method == 'POST':
+        global product, price
+
+        product = request.form['product']
+        price = request.form['price']
+        
+        mycursor.execute("INSERT INTO shop (product, price) VALUES (%s ,%s)",(product, price))
+        
+        mydb.commit()
+
+        
+        print("record inserted.")
+    else:
+        print("NOT RECORDED!!")
+
+
+#---------SHOW DATA FRAM DATABASES------
+
