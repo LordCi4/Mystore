25b1f80f4d9d4d78e44e873164c56ade08c409ae
LordCi4 <ramosubaldino5@gmail.com>
2023-08-21 20:52:49 -0500

    modified:   appPy/__pycache__/app.cpython-310.pyc 	modified:   appPy/app.py 	modified:   appPy/templates/add.html 	modified:   appPy/templates/index.html

--- appPy/app.py
+++ appPy/app.py
@@ -13,16 +13,9 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM shop")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-    print(x)
-
 #------ADD VAlUES TO DATABASE
 
-@app.route("/add", methods = ['POST', 'GET'])
+@app.route("/", methods = ['POST', 'GET'])
 def add():
     adding()
     return render_template("add.html")
@@ -40,10 +33,34 @@ def adding():
         mydb.commit()
 
         
-        print("record inserted.")
+        return render_template("index.html")
     else:
-        print("NOT RECORDED!!")
+        return render_template("add.html")
+
 
 
 #---------SHOW DATA FRAM DATABASES------
+@app.route("/index", methods = ['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+        mycursor.execute('SELECT * FROM shop')    
+        
+        result = mycursor.fetchall()
 
+        mycursor.execute('SELECT price FROM shop')
+        
+        total = sum(value[0] for value in mycursor)
+
+        return render_template('index.html', result = result, total=total)
+   
+    """if request.method == 'GET':
+        sumPrice = mycursor.execute('SELECT price FROM shop')
+
+        for x in range(sumPrice):
+            total = total + x
+
+        
+
+        return render_template('index.html', total = total)"""
+
+    
